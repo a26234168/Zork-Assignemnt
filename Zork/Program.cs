cc49fa5b8006fef4fcd642a59716109371cf8bc0
a26234168 <57016191+a26234168@users.noreply.github.com>
2021-09-01 15:20:33 -0400

    school work for 1.3 not yet done

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,10 +7,36 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork");
-
+            Commands command = Commands.UNKNOWN;
+            while (Commands != Commands.QUIT) ;
             string inputString = Console.ReadLine();
             Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            string outputString;
+            
+            switch (command)
+            {
+                case Commands.QUIT:
+                    outputString = "Thank you for playing.";
+                    break;
+                case Commands.LOOK:
+                    outputString = "This is anopen field west of a white house,with a boarded front door.\n Arubber mat saying'Welcome to Zork' lies by the door.";
+
+                    break;
+                case Commands.NORTH:                    
+                case Commands.SOUTH:                    
+                case Commands.EAST:                    
+                case Commands.WEST:
+                    outputString = $"you move {command}.";
+                    break;
+                
+                   
+                default:
+                    outputString = "Unrecognized command";
+                    break;
+
+
+            }
+            Console.WriteLine(outputString);
         }
         private static Commands ToCommand(string commandString)
         {
