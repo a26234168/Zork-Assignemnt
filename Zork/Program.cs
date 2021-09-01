55f44b658f69febbcbcb778d43115dbd635929cd
a26234168 <57016191+a26234168@users.noreply.github.com>
2021-09-01 14:47:19 -0400

    Zork1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,19 +9,12 @@ namespace Zork
             Console.WriteLine("Welcome to Zork");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing");
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is anopen field west of a white house,with a boarded front door.\n Arubber mat saying'Welcome to Zork' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+        }
+        private static Commands ToCommand(string commandString)
+        {
+            return Enum.TryParse<Commands>(commandString, out Commands command) ? command : Commands.UNKNOWN;
         }
     }
 }
