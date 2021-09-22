267b877e60477d5561605fa07e5a29d2fe216670
a26234168 <57016191+a26234168@users.noreply.github.com>
2021-09-21 23:00:29 -0400

    some work from home

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -14,7 +14,7 @@ namespace Zork
         }
         static void Main(string[] args)
         {
-            initial
+            InitializeRoomDescriptions();
             Console.WriteLine("Welcome to Zork");
 
             Commands command = Commands.UNKNOWN;
@@ -83,7 +83,19 @@ namespace Zork
             return didMove;
         }
 
-        private static void Initialis
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strwn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with house, with a boarded front door";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly jar";
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south";
+
+        }
         private static readonly Room[,] Rooms = {
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
             { new Room("Forest"), new Room("West of House"), new Room("Behind House")},
