a4c274528d3539af47d0211d901c32d06b3f1649
a26234168 <57016191+a26234168@users.noreply.github.com>
2021-09-26 22:36:23 -0400

    Zork3.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Zork
 {
@@ -15,12 +17,20 @@ namespace Zork
         static void Main(string[] args)
         {
             InitializeRoomDescriptions();
+            Room previousRoom = null;
+
             Console.WriteLine("Welcome to Zork");
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.Write($"{Location.Name}\n> ");
+                Console.WriteLine(Location.Name);
+                if (previousRoom != Location)
+                {
+                    Console.WriteLine(Location.Description);
+                    previousRoom = Location;
+                }
+                Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim().ToUpper());
 
                 string outputString;
@@ -30,7 +40,7 @@ namespace Zork
                         outputString = "Thank you for playing.";
                         break;
                     case Commands.LOOK:
-                        outputString = (Location.Description);                         
+                        outputString = (Location.Description);
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
@@ -82,15 +92,20 @@ namespace Zork
 
         private static void InitializeRoomDescriptions()
         {
-            Rooms[0, 0].Description = "You are on a rock-strwn trail.";
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
-            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
-            Rooms[1, 1].Description = "This is an open field west of a white house, with house, with a boarded front door";
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly jar";
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred";
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south";
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in Rooms)
+            {
+                roomMap.Add(room.Name, room);
+            }
+            roomMap["Rocky Trail"].Description = "You are on a rock-strwn trail.";
+            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with house, with a boarded front door";
+            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly jar";
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred";
+            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south";
 
         }
         private static readonly Room[,] Rooms = {
